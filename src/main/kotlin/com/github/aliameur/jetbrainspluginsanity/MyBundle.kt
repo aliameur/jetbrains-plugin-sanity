5e0a76da63d5e23c62e775c2ec77ed6f72a7d173
GitHub Action <action@github.com>
2024-11-06 14:59:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/aliameur/jetbrainspluginsanity/MyBundle.kt
+++ src/main/kotlin/com/github/aliameur/jetbrainspluginsanity/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.aliameur.jetbrainspluginsanity
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/aliameur/jetbrainspluginsanity/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/aliameur/jetbrainspluginsanity/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.aliameur.jetbrainspluginsanity.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
